a9b48e18ee2bd404de3ba213f9d5702a2f423459
adhnan <adhnan.e@equipohealth.com>
2025-06-05 14:50:16 +0530

    v2

--- lib/steps/drawer/step_background_drawer.dart
+++ lib/steps/drawer/step_background_drawer.dart
@@ -1,10 +1,10 @@
 // lib/steps/drawer/step_background_drawer.dart
 
-// Re-export the shared background drawer
-export '../../shared/drawers/chart_background_drawer.dart';
-
 // For backwards compatibility, create an alias
 import '../../shared/drawers/chart_background_drawer.dart';
+
+// Re-export the shared background drawer
+export '../../shared/drawers/chart_background_drawer.dart';
 
 /// Alias for the shared chart background drawer
 typedef StepBackgroundDrawer = ChartBackgroundDrawer;
